ff0cbe79032ae4ea992d0f7ecd13a3e9b8c36b92
tadpolecom <2lyydiaekroos97@gmail.com>
2021-06-03 21:35:35 +0900

    not enter

--- main.py
+++ main.py
@@ -105,10 +105,12 @@ def check_messege(event):
         enter = []
         if ((len(shift_data) - 1) % 3) == 0:
             for i in range(1,round((len(shift_data) - 1) / 3)):
-                enter.append([shift_data[0],shift_data[i+1],shift_data[i+2],shift_data[i+3]])
+                enter.append([shift_data[0], shift_data[i + 1], shift_data[i + 2], shift_data[i + 3]])
+            print(enter)
             with get_connection as conn:
                 with conn.cursor() as cur:
-                    cur.execute('INSERT INTO shift_table (id,date,start,last) VALUES (%s, %s, %s, %s)',enter)
+                    for i in range(1,round((len(shift_data) - 1) / 3)):
+                        cur.execute('INSERT INTO shift_table (id,date,start,last) VALUES (%s, %s, %s, %s)', [shift_data[0], shift_data[i + 1], shift_data[i + 2], shift_data[i + 3]])
                 conn.commit()
         else:
             abort(400)
