621cf1e131312a6d5813a74ac491ef3ae941235d
tadpolecom <2lyydiaekroos97@gmail.com>
2021-06-03 20:22:29 +0900

    1page fix

--- main.py
+++ main.py
@@ -14,6 +14,56 @@ import psycopg2
 import xlsxwriter
 
 
+class shift:
+    def __init__(self):
+        self.get_connection = psycopg2.connect(os.environ.get('DATABASE_URL'))
+
+    def insert(self, data):
+        enter=[]
+        for i in range(1,len(data)):
+            enter.append([data[0],data[i+1],data[i+2],data[i+3]])
+
+        with self.get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute('INSERT INTO shift_table (id,date,start,last) VALUES (%s, %s, %s, %s)',data)
+            conn.commit()
+
+    def into_xlsx(self):
+        with self.get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute('SELECT * FROM member_tb', ('foo',))
+                rows = cur.fetchall()
+        print(rows)
+
+        tmp = []
+        data = []
+        for item in rows:
+            tmp.append(item)
+            for item in tmp:
+                data.append(tmp)
+
+        rowNum = 0
+        workbook = xlsxwriter.Workbook('fileName' + '.xlsx')
+        worksheet = workbook.add_worksheet('issues')
+        rowNum = 0
+        for tmp in rows:
+            j = 0
+            for item in tmp:
+                worksheet.write(rowNum, j, item)
+                j += 1
+            rowNum += 1
+        workbook.close()
+
+        DOWNLOAD_DIR_PATH = "."
+        downloadFileName = "test.xlsx"
+        downloadFile = "fileName.xlsx"
+
+        return send_from_directory(DOWNLOAD_DIR_PATH, downloadFile, \
+            as_attachment = True, attachment_filename = downloadFileName, \
+            mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+            
+
+
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ['YOUR_CHANNEL_ACCESS_TOKEN'])
@@ -152,53 +202,3 @@ def handle_postback(event):
 #herokuにおいてポートはランダムに環境変数PORTで決められる
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0', port=os.environ.get('PORT'))
-
-
-
-class shift:
-    def __init__(self):
-        self.get_connection = psycopg2.connect(os.environ.get('DATABASE_URL'))
-
-    def insert(self, data):
-        enter=[]
-        for i in range(1,len(data)):
-            enter.append([data[0],data[i+1],data[i+2],data[i+3]])
-
-        with self.get_connection() as conn:
-            with conn.cursor() as cur:
-                cur.execute('INSERT INTO shift_table (id,date,start,last) VALUES (%s, %s, %s, %s)',data)
-            conn.commit()
-
-    def into_xlsx(self):
-        with self.get_connection() as conn:
-            with conn.cursor() as cur:
-                cur.execute('SELECT * FROM member_tb', ('foo',))
-                rows = cur.fetchall()
-        print(rows)
-
-        tmp = []
-        data = []
-        for item in rows:
-            tmp.append(item)
-            for item in tmp:
-                data.append(tmp)
-
-        rowNum = 0
-        workbook = xlsxwriter.Workbook('fileName' + '.xlsx')
-        worksheet = workbook.add_worksheet('issues')
-        rowNum = 0
-        for tmp in rows:
-            j = 0
-            for item in tmp:
-                worksheet.write(rowNum, j, item)
-                j += 1
-            rowNum += 1
-        workbook.close()
-
-        DOWNLOAD_DIR_PATH = "."
-        downloadFileName = "test.xlsx"
-        downloadFile = "fileName.xlsx"
-
-        return send_from_directory(DOWNLOAD_DIR_PATH, downloadFile, \
-            as_attachment = True, attachment_filename = downloadFileName, \
-            mimetype = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
