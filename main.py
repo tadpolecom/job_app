404328a5be305320e133c9ee77a9f370b512f61d
tadpolecom <2lyydiaekroos97@gmail.com>
2021-06-03 21:24:05 +0900

    not class2

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ shift_data=[]
 
 @app.route("/")
 def main():
-    with get_connection() as conn:
+    with get_connection as conn:
         with conn.cursor() as cur:
             cur.execute('SELECT * FROM member_tb', ('foo',))
             rows = cur.fetchall()
@@ -106,7 +106,7 @@ def check_messege(event):
         if ((len(shift_data) - 1) % 3) == 0:
             for j in range(1,round((len(shift_data) - 1) / 3)):
                 enter.append([shift_data[0],shift_data[i+1],shift_data[i+2],shift_data[i+3]])
-            with get_connection() as conn:
+            with get_connection as conn:
                 with conn.cursor() as cur:
                     cur.execute('INSERT INTO shift_table (id,date,start,last) VALUES (%s, %s, %s, %s)',enter)
                 conn.commit()
