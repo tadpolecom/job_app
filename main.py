ee734453ed5dc47733cfbdd2c12d9c63ad478df8
tadpolecom <2lyydiaekroos97@gmail.com>
2021-06-03 20:18:55 +0900

    1page

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, request, abort
+from flask import Flask, request, abort, render_template, request, redirect, send_from_directory
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -10,8 +10,9 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ConfirmTemplate,DatetimePickerAction,MessageAction,PostbackEvent
 )
+import psycopg2
+import xlsxwriter
 
-from db import shift
 
 app = Flask(__name__)
 
@@ -23,8 +24,8 @@ shift_data=[]
 
 @app.route("/")
 def main():
-    
-    return shift().into_xlsx()
+    shift = shift()
+    return shift.into_xlsx()
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -69,7 +70,8 @@ def check_messege(event):
         )
     elif event.message.text == "終了":
         line_bot_api.reply_message(event.reply_token, TextSendMessage('thank you'))
-        shift().insert(shift_data)
+        shift = shift()
+        shift.insert(shift_data)
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage('もう一度「シフトを提出」と入力してください'))
     
@@ -150,3 +152,53 @@ def handle_postback(event):
 #herokuにおいてポートはランダムに環境変数PORTで決められる
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0', port=os.environ.get('PORT'))
+
+
+
+class shift:
+    def __init__(self):
+        self.get_connection = psycopg2.connect(os.environ.get('DATABASE_URL'))
+
+    def insert(self, data):
+        enter=[]
+        for i in range(1,len(data)):
+            enter.append([data[0],data[i+1],data[i+2],data[i+3]])
+
+        with self.get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute('INSERT INTO shift_table (id,date,start,last) VALUES (%s, %s, %s, %s)',data)
+            conn.commit()
+
+    def into_xlsx(self):
+        with self.get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute('SELECT * FROM member_tb', ('foo',))
+                rows = cur.fetchall()
+        print(rows)
+
+        tmp = []
+        data = []
+        for item in rows:
+            tmp.append(item)
+            for item in tmp:
+                data.append(tmp)
+
+        rowNum = 0
+        workbook = xlsxwriter.Workbook('fileName' + '.xlsx')
+        worksheet = workbook.add_worksheet('issues')
+        rowNum = 0
+        for tmp in rows:
+            j = 0
+            for item in tmp:
+                worksheet.write(rowNum, j, item)
+                j += 1
+            rowNum += 1
+        workbook.close()
+
+        DOWNLOAD_DIR_PATH = "."
+        downloadFileName = "test.xlsx"
+        downloadFile = "fileName.xlsx"
+
+        return send_from_directory(DOWNLOAD_DIR_PATH, downloadFile, \
+            as_attachment = True, attachment_filename = downloadFileName, \
+            mimetype = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
