e5672af9c98d831f2bbfe328be8cfb1f8484378c
tadpolecom <2lyydiaekroos97@gmail.com>
2021-06-03 23:40:00 +0900

    comp2

--- main.py
+++ main.py
@@ -105,9 +105,9 @@ def check_messege(event):
         enter = []
         
         if ((len(shift_data) - 1) % 3) == 0:
-            for i in range(1,round((len(shift_data) - 1) / 3)):
+            for i in range(round((len(shift_data) - 1) / 3)):
                 enter.append(shift_data[0], shift_data[i + 1], shift_data[i + 2], shift_data[i + 3])
-            print(enter)
+            print(enter+','+round((len(shift_data) - 1) / 3))
 
             #test = [[8, 7, 6, 5], [4, 3, 2, 1]]
             with get_connection as conn:
