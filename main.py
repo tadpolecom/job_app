baaa73c7476dbe5edb2577cf015f7b4d6f2e70bc
tadpolecom <2lyydiaekroos97@gmail.com>
2021-06-03 23:42:57 +0900

    comp3

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ def check_messege(event):
         
         if ((len(shift_data) - 1) % 3) == 0:
             for i in range(round((len(shift_data) - 1) / 3)):
-                enter.append(shift_data[0], shift_data[i + 1], shift_data[i + 2], shift_data[i + 3])
+                enter.append([shift_data[0], shift_data[i + 1], shift_data[i + 2], shift_data[i + 3]])
             print(enter+','+round((len(shift_data) - 1) / 3))
 
             #test = [[8, 7, 6, 5], [4, 3, 2, 1]]
